abe18d24b4eee070d6f399223acc89fe01789852
ziJinChampion <2932447829@qq.com>
2022-09-29 21:08:19 +0800

    [southwind]feat: init project

---
+++ talk-service/src/test/java/com/sw/talkservice/TalkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.talkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
