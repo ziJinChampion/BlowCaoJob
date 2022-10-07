b6d2d95010c7a2662d0d29caea5e02319f95b998
ziJinChampion <2932447829@qq.com>
2022-10-07 13:29:21 +0800

    [southwind] feat: add datasource dependency to common

--- common/src/main/java/com/sw/common/pojo/Result.java
+++ common/src/main/java/com/sw/common/pojo/Result.java
@@ -1,5 +1,6 @@
 package com.sw.common.pojo;
 
+
 public class Result<T> {
 
     private Boolean flag;
@@ -7,10 +8,22 @@ public class Result<T> {
     private String message;
     private T data;
 
-    public Result(Boolean flag, Integer code, String message, T data) {
+    public Result(Boolean flag, Integer code, String message, Object data) {
         this.flag = flag;
         this.code = code;
         this.message = message;
-        this.data = data;
+        this.data = (T) data;
+    }
+
+    public Result(Boolean flag, Integer code, String message) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+    }
+
+    public Result(){
+        this.flag = true;
+        this.code = StatusCode.OK;
+        this.message = "success";
     }
 }
